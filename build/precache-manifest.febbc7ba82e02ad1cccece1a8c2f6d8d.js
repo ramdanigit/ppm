8012766b874d6b9db6818de1ab74c9dc62d74676
NovalMF <novalfaisal26@gmail.com>
2020-08-06 20:13:28 +0700

    commit build

---
+++ build/precache-manifest.febbc7ba82e02ad1cccece1a8c2f6d8d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3c7784d6c4854a44fb61ce517a836d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cada1152963855a11d0e",
+    "url": "/static/css/2.c90e42fa.chunk.css"
+  },
+  {
+    "revision": "c6da3fb1fd90ad49367a",
+    "url": "/static/css/main.ace05b41.chunk.css"
+  },
+  {
+    "revision": "cada1152963855a11d0e",
+    "url": "/static/js/2.4e94dde1.chunk.js"
+  },
+  {
+    "revision": "fff6b400a90c1627011affefa34113ee",
+    "url": "/static/js/2.4e94dde1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6da3fb1fd90ad49367a",
+    "url": "/static/js/main.cfe72f6c.chunk.js"
+  },
+  {
+    "revision": "542501077ba3f4b73a33",
+    "url": "/static/js/runtime-main.051f1400.js"
+  },
+  {
+    "revision": "f382fa5d7c2fb2ce6779e4f1461e0f54",
+    "url": "/static/media/akun.f382fa5d.svg"
+  },
+  {
+    "revision": "b91597a8f5d2e769e880d106e9d34acb",
+    "url": "/static/media/hero.b91597a8.png"
+  },
+  {
+    "revision": "6410889cd76600252580e3502d868cb4",
+    "url": "/static/media/hero01.6410889c.png"
+  },
+  {
+    "revision": "aee46b2aecf71b2ffe567f2959b65203",
+    "url": "/static/media/hero03.aee46b2a.png"
+  },
+  {
+    "revision": "e00dff035b6c7975024188166d3f3288",
+    "url": "/static/media/logo.e00dff03.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c6cae421d16972f404efd9c8f304be30",
+    "url": "/static/media/test.c6cae421.jpg"
+  }
+]);
